4adee5f0b9279a8857c8572670d3f6688ef3d161
scottmurphy1111 <scottmurphy1111@gmail.com>
2018-06-20 21:26:25 -0400

    initial commit

---
+++ src/pages/cartpage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CartPage(props) {
+    return <div>
+        <h2>CartPage</h2>
+    </div>
+}
---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductListing from '../features/product-listing';
+
+import data from '../data/products.json';
+
+export default function Homepage(props) {
+    return <div>
+        <h2>Homepage</h2>
+        <ProductListing products={data.products} />
+    </div>
+}
